ed74db6c6441eecaa19cdf437ed8e4e6809e54e0
lior2911 <aytegev1@gmail.com>
2022-07-06 21:45:02 +0300

    lastlast

---
+++ pages/javascript/userData.js
@@ -0,0 +1,33 @@
+async function getUserList() {
+  let userAPI = "https://my-json-server.typicode.com/Jeck99/fake-server/users";
+  try {
+    return await fetch(userAPI).then(response => 
+      response.json()
+    )
+  } catch (error) {
+    console.log("error")
+  } finally {
+  }
+}
+
+function printToTable() {
+  let myTable = document.getElementById("userData")
+  getUserList()
+  .then((res)=>{
+    res.forEach(element => {
+      myTable.innerHTML += `
+      <tr>
+      <td class='border-2'>${element.age}</td>
+      <td class='border-2'>${element.name.first}</td>
+      <td class='border-2'>${element.name.last}</td>
+      <td class='border-2'>${element.email}</td>
+      <td class='border-2'>${element.phone}</td>
+      </tr>
+      `
+      
+      
+    })
+   
+  })
+}
+printToTable()

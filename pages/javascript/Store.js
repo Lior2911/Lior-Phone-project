5ff0dcbf9fe242dda0d2d6e7ce1438cc36a43e80
lior2911 <aytegev1@gmail.com>
2022-07-06 17:29:57 +0300

    bigCommit

--- pages/javascript/Store.js
+++ pages/javascript/Store.js
@@ -1,24 +1,27 @@
 const WEBAPI = "https://my-json-server.typicode.com/Jeck99/fake-server/devices";
-const MY_IMG = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_B7JYHlbfR2P2nV5eK7z5oD59A3rhbF1jag&usqp=CAU";
-const counter = 0;
+const MY_IMG =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_B7JYHlbfR2P2nV5eK7z5oD59A3rhbF1jag&usqp=CAU";
+const phoneCard_ = document.getElementById("phoneCard");
 async function getFromApi() {
   try {
     return await fetch(WEBAPI)
       .then((response) => response.json())
       .then((res) =>
         res.forEach((element) => {
-          phoneCard.innerHTML += `<div class='col-sm-4 col-md-4 text-light border border-primary rounded bg-dark bg-opacity-25'>
-         <div >
-         <img class="img-card col-md-12 col-sm-12" src=' ${MY_IMG}'/>
-         </div>
-      <p>price :${element.price}<br>
-      isAvailable :${element.isAvailable}<br
-      createdAt :${element.createdAt}<br>
-      color :${element.color}<br>
-      brand :${element.brand}<br>
-      ram :${element.ram}</p>
+          phoneCard_.innerHTML += `<div class='col-xs-3 border-2 p-2 col-sm-4
+           bg-dark bg-opacity mb-1 text-light'>
       
-      <div>`;
+     <img class="w-100 img-thumbnail"  src='${MY_IMG}'/>
+  
+      <div class='fs-6'>price : ${element.price}</div>
+      <div>isAvailable :${element.isAvailable}</div>
+      <div>createdAt :${element.createdAt}</div>
+      <div>color :${element.color}</div>
+      <div>brand :${element.brand}</div>
+      <div>ram :${element.ram}</div>
+    
+    
+      </div>`;
         })
       );
   } catch (error) {
